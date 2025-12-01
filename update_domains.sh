2999619fb78447c5371e1ce8d4f015e179e5e35c
ImMALWARE <contact@malw.link>
2025-12-01 18:14:39 +0300

    Clear logs on update

--- update_domains.sh
+++ update_domains.sh
@@ -1,4 +1,5 @@
 docker-compose down
+rm logs -rf
 curl -L -o "proxy.txt" "https://github.com/ImMALWARE/dns.malw.link/raw/refs/heads/master/domains.txt"
 curl -L -o "proxy_with_subdomains.txt" "https://github.com/ImMALWARE/dns.malw.link/raw/refs/heads/master/domains_with_subdomains.txt"
 docker-compose up -d --build
